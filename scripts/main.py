0b8c05e13e13548d219f4c73f7573b89d447b608
newhandnew <easytired@gmail.com>
2015-10-30 10:02:46 +0800

    combine zbar decoder and update launch file

--- scripts/main.py
+++ scripts/main.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python
 
 import rospy
+import time
 from std_msgs.msg import String
+from zbar_ros.msg import Marker
 
 
 class mainSystem(object):
 	def __init__(self):
 		rospy.init_node('mainSystem')
-		rospy.Subscriber("barcodeData", String, self.handleBarcodeData)  # Is this line or the below bad redundancy?
+		rospy.Subscriber("markers", Marker, self.handleBarcodeData)  # Is this line or the below bad redundancy?
 		self.ericaCmdPublisher = rospy.Publisher('cmd_erica', String, queue_size=5)
 		self.ttsPublisher = rospy.Publisher('ttsText', String, queue_size=5)
-    	rospy.loginfo("Main system started.")
+		rospy.loginfo("Main system started.")
+		self.startTime = 0
 
 	def handleBarcodeData(self, data):
-		print data.data
+		rospy.loginfo("get barcode %d and %d : " + data.data, self.startTime, time.time())
 		self.dataInput = data.data.split(",")
+		# barcode format must meet 4 column
+		# set time interval to 3 seconds
 		if (len(self.dataInput) == 4):
-			self.ttsPublisher.publish(String(str(self.dataInput[2])))
-			self.ericaCmdPublisher.publish(String(str(self.dataInput[3])))
+			if ((time.time() - self.startTime) > 3):
+				self.startTime = time.time();
+				self.ttsPublisher.publish(String(str(self.dataInput[2])))
+				self.ericaCmdPublisher.publish(String(str(self.dataInput[3])))
 
 if __name__ == '__main__':
 
